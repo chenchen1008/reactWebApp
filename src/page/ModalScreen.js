4284110c26b836da75fbc8779c4a75ec1d2bb6dd
guominghao <gmhqyj@163.com>
2018-01-07 16:30:41 +0800

    增加modal样例

--- src/page/ModalScreen.js
+++ src/page/ModalScreen.js
@@ -13,17 +13,27 @@ class ModalScreen extends Component {
         super(props);
         this.state = {
             data: [],
-            isOpen:false,
+            isOpen: false,
         };
     }
 
     render() {
 
         return (
-            <div style={{backgroundColor: '#ff00ff', display: 'flex',flexDirection:'column',height:'100vh',}}>
-                <div style={{backgroundColor: '#00ffff',height:44}} onClick={()=>{this.setState({isOpen:!this.state.isOpen})}}>
+            <div style={{backgroundColor: '#ff00ff', display: 'flex', flexDirection: 'column', height: '100vh',}}>
+                <div style={{backgroundColor: '#00ffff', height: 44}} onClick={() => {
+                    this.setState({isOpen: !this.state.isOpen})
+                }}>
                 </div>
-                <Modal visiable={this.state.isOpen} style ={{position:'fixed',left:0,top:0,width:100,height:100,backgroundColor:'#456',opacity:0.2}}>hello</Modal>
+                <Modal visiable={this.state.isOpen} onClick={()=>this.setState({isOpen: !this.state.isOpen})} style={{
+                    position: 'fixed',
+                    left: 0,
+                    top: 0,
+                    width: 100,
+                    height: 100,
+                    backgroundColor: '#456',
+                    opacity: 0.2
+                }}>hello</Modal>
             </div>
         );
     }
